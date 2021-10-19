b0b5c9f75a7130d5563732e9f4518198cb8420cc
FreelancerLifeStyle <info@fls.guru>
2021-10-19 10:07:16 +0300

    new

--- index.js
+++ index.js
@@ -1,66 +1,64 @@
-const gutil = require('gulp-util');
-const PluginError = gutil.PluginError;
-const through = require('through2');
+const Vinyl = require('vinyl')
+const PluginError = Vinyl.PluginError
+const through = require('through2')
 
-const pluginName = 'gulp-webp-in-html';
+const pluginName = 'gulp-webp-html-nosvg'
 
-module.exports = function () {
-	var extensions = ['.jpg', '.png', '.gif', '.GIF', '.jpeg', '.JPG', '.PNG', '.JPEG'];
+module.exports = function (extensions) {
+	var extensions = ['.jpg', '.png', '.jpeg', '.GIF', '.gif', '.JPG', '.PNG', '.JPEG'];
 	return through.obj(function (file, enc, cb) {
 		if (file.isNull()) {
-			cb(null, file);
-			return;
+			cb(null, file)
+			return
 		}
 		if (file.isStream()) {
-			cb(new PluginError(pluginName, 'Streaming not supported'));
-			return;
+			cb(new PluginError(pluginName, 'Streaming not supported'))
+			return
 		}
 		try {
-			var SplitPicture = '<picture>';
-			var RePicture = /([\s\S]*?<\/picture>)([\s\S]*)/;
-			var SplitImg = '<img ';
-			var ReImg = /<img([^>]*)src=[\"\'](\S+)[\"\']([^>]*)>/;
-			//
-			const data = file.contents.toString()
-				.split(SplitPicture)
+			var pictureRender = function (url, imgTag) {
+				return (
+					'<picture><source srcset="' +
+					url +
+					'" type="image/webp">' +
+					imgTag +
+					'</picture>'
+				)
+			}
+			var inPicture = false
+			const data = file.contents
+				.toString()
+				.split('\n')
 				.map(function (line) {
-					var picture = '';
-					if (RePicture.test(line)) {
-						var lineA = line.match(RePicture);
-						picture = lineA[1];
-						line = lineA[2];
+					// Вне <picture/>?
+					if (line.indexOf('<picture') + 1) inPicture = true
+					if (line.indexOf('</picture') + 1) inPicture = false
+
+					// Проверяем есть ли <img/>
+					if (line.indexOf('<img') + 1 && !inPicture) {
+						// Новый урл с .webp
+						var Re = /<img([^>]*)src=\"(\S+)\"([^>]*)>/gi
+						var regexpArray = Re.exec(line)
+						var imgTag = regexpArray[0]
+						var newUrl = regexpArray[2]
+						// Если в урле есть .webp или .svg, пропускаем
+						if (newUrl.indexOf('.webp') + 1 || newUrl.indexOf('.svg') + 1) return line
+						// Заменяем все расширения на .webp
+						for (k in extensions) {
+							newUrl = newUrl.replace(extensions[k], '.webp')
+						}
+						// Компилим <picture/>
+						var newHTML = pictureRender(newUrl, imgTag)
+						return line.replace(imgTag, newHTML)
 					}
-					if (~line.indexOf(SplitImg)) {
-						var lineNew = line
-							.split(SplitImg)
-							.map(function (subLine) {
-								var lineImg = (SplitImg + subLine).toString();
-								if (ReImg.test(lineImg)) {
-									var regexpArray = lineImg.match(ReImg);
-									var imgTag = regexpArray[0];
-									var newUrl = regexpArray[2];
-									if (newUrl.indexOf('.webp') < 0) {
-										for (var k in extensions) {
-											newUrl = newUrl.replace(extensions[k], '.webp');
-										}
-										var newHTML = '<picture><source srcset="' + newUrl + '" type="image/webp">' + imgTag + '</picture>';
-										subLine = lineImg.replace(imgTag, newHTML);
-									}
-								}
-								return subLine;
-							})
-							.join('');
-						line = lineNew;
-					}
-					return picture + line;
+					return line
 				})
-				.join(SplitPicture);
-			//
-			file.contents = new Buffer(data);
-			this.push(file);
+				.join('\n')
+			file.contents = new Buffer.from(data)
+			this.push(file)
 		} catch (err) {
-			this.emit('error', new PluginError(pluginName, err));
+			this.emit('error', new PluginError(pluginName, err))
 		}
-		cb();
-	});
-};
+		cb()
+	})
+}
